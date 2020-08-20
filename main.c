4753b422e590e1d7c0d7b0862bac8c25b8657282
Clayton Zambon <zambon@outlook.com.br>
2020-08-19 21:04:18 -0300

    Adicionadas novas mensagens

--- main.c
+++ main.c
@@ -8,5 +8,7 @@ int main()
     printf("Astalavista\n");
     printf("Tchau belo!!\n");
     printf("Mensagem para aparecer no GitHub\n");
+    printf("Mais uma mensagem\n");
+    printf("Mais outra mensagem\n");
     return 0;
 }
